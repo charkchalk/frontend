bf8ef37c0631d6e73910575da6befe4bb6cd1749
moontai0724 <moontai0724@gmail.com>
2023-01-19 22:59:17 +0800

    fix: change course route to courses

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
   {
-    path: "course",
+    path: "courses",
     loadChildren: () =>
       import("./course/course.module").then(m => m.CourseModule),
   },
