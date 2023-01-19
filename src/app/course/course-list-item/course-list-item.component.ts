05e2ca4829f0d2c4b824fb77464bca6b7e1b9854
moontai0724 <moontai0724@gmail.com>
2023-01-19 21:25:07 +0800

    feat: copy course text to clipboard

--- src/app/course/course-list-item/course-list-item.component.ts
+++ src/app/course/course-list-item/course-list-item.component.ts
@@ -1,4 +1,6 @@
+import { Clipboard } from "@angular/cdk/clipboard";
 import { Component, Input } from "@angular/core";
+import { MatSnackBar } from "@angular/material/snack-bar";
 
 @Component({
   selector: "app-course-list-item",
@@ -7,4 +9,46 @@ import { Component, Input } from "@angular/core";
 })
 export class CourseListItemComponent {
   @Input() course?: RawCourse;
+
+  constructor(private _snackBar: MatSnackBar, private clipboard: Clipboard) {}
+
+  copyCourseCode() {
+    if (!this.course?.code) return;
+
+    this.clipboard.copy(this.course.code);
+
+    this._snackBar.open("已經複製到剪貼簿囉～", "關閉", {
+      duration: 1000,
+      horizontalPosition: "center",
+      verticalPosition: "top",
+    });
+  }
+
+  copyCourseText() {
+    const courseText = [
+      `我在看選課代號是 ${this.course?.code}，`,
+      `由 `,
+      this.course?.hosts.map(host => host.name).join(" 跟 "),
+      ` 開給 ${this.course?.organization.name} 的「${this.course?.name}」。`,
+      `${this.course?.type.name} ${this.course?.credit} 學分，`,
+      `每週 `,
+      this.course?.timeRanges
+        .map(
+          timeRange =>
+            `${timeRange.day} ${timeRange.start_time} ~ ${timeRange.end_time}`,
+        )
+        .join(", "),
+      ` 在 `,
+      this.course?.places.map(place => place.name).join(" 跟 "),
+      ` 上課。`,
+    ].join("");
+
+    this.clipboard.copy(courseText);
+
+    this._snackBar.open("已經複製到剪貼簿囉～", "關閉", {
+      duration: 1000,
+      horizontalPosition: "center",
+      verticalPosition: "top",
+    });
+  }
 }
--- src/app/course/course-list-item/course-list-item.module.ts
+++ src/app/course/course-list-item/course-list-item.module.ts
@@ -3,12 +3,19 @@ import { NgModule } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
 import { MatIconModule } from "@angular/material/icon";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
 
 import { CourseListItemComponent } from "./course-list-item.component";
 
 @NgModule({
   declarations: [CourseListItemComponent],
-  imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSnackBarModule,
+  ],
   exports: [CourseListItemComponent],
 })
 export class CourseListItemModule {}
