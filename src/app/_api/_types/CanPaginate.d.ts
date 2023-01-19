c56ea66e575b3c1be5460dc1bc158d4583096f8b
moontai0724 <moontai0724@gmail.com>
2023-01-19 16:24:40 +0800

    feat: migrate course list page with http api

---
+++ src/app/_api/_types/CanPaginate.d.ts
@@ -0,0 +1,6 @@
+interface CanPaginate {
+  /** number of records to skip for pagination */
+  skip?: number;
+  /** maximum number of records to return */
+  limit?: number;
+}
---
+++ src/app/_api/course/course.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { map, Observable } from "rxjs";
+
+@Injectable({
+  providedIn: "root",
+})
+export class CourseService {
+  private _uri = "/courses";
+
+  constructor(private http: HttpClient) {}
+
+  search(options?: CanPaginate): Observable<RawCourse[]> {
+    const params = new HttpParams();
+    if (options) {
+      for (const [key, value] of Object.entries(options)) {
+        params.set(key, value);
+      }
+    }
+
+    return this.http
+      .get<StandardResponse<RawCourse[]>>(this._uri, {
+        responseType: "json",
+        params: params,
+      })
+      .pipe(map(response => response.data));
+  }
+}
