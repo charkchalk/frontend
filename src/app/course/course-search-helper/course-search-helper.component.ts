04f6c2638e39df7f1b2d85a4c00ccd732efbd639
moontai0724 <moontai0724@gmail.com>
2023-02-24 23:08:45 +0800

    feat: query key and method selection

--- src/app/course/course-search-helper/course-search-helper.component.ts
+++ src/app/course/course-search-helper/course-search-helper.component.ts
@@ -1,18 +1,38 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 
-import { CourseQueryManagerService } from "../_query/course-query-manager.service";
+import { Displayable } from "../../_types/displayable";
+import { CourseQueryService } from "../_query/course-query.service";
+import { QueryDataProvider } from "../_query/query-data-provider";
 
 @Component({
   selector: "app-course-search-helper",
   templateUrl: "./course-search-helper.component.html",
   styleUrls: ["./course-search-helper.component.css"],
 })
-export class CourseSearchHelperComponent {
+export class CourseSearchHelperComponent implements OnInit {
   @Input() index!: number;
+  providers: Displayable[] = [];
+  query: QueryItem = {};
 
-  constructor(private courseQueryManagerService: CourseQueryManagerService) {}
+  provider?: QueryDataProvider;
+  methods?: Displayable[];
+  options?: Displayable[];
+
+  constructor(private courseQueryService: CourseQueryService) {}
+
+  ngOnInit() {
+    this.providers = this.courseQueryService
+      .getProviders()
+      .map(provider => ({ key: provider.key, label: provider.label }));
+    this.query = this.courseQueryService.getQuery(this.index);
+  }
+
+  setProvider(providerKey: string) {
+    this.provider = this.courseQueryService.getProvider(providerKey);
+    this.methods = this.provider?.getMethods();
+  }
 
   removeQuery() {
-    this.courseQueryManagerService.removeQuery(this.index);
+    this.courseQueryService.removeQuery(this.index);
   }
 }
