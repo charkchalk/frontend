d8e2dd100250a78cfe077f8676c6713690025ecc
moontai0724 <moontai0724@gmail.com>
2023-05-07 16:38:09 +0800

    fix: replace helper text input w/ prime

--- src/app/course/course-search-helper/text-input/text-input.component.ts
+++ src/app/course/course-search-helper/text-input/text-input.component.ts
@@ -1,14 +1,4 @@
-import { ENTER, SPACE } from "@angular/cdk/keycodes";
-import {
-  Component,
-  EventEmitter,
-  Input,
-  OnInit,
-  Output,
-  ViewChild,
-} from "@angular/core";
-import { FormControl } from "@angular/forms";
-import { MatChipGrid, MatChipInputEvent } from "@angular/material/chips";
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 
 import { Displayable } from "../../../_types/displayable";
 import { QueryDataProvider } from "../../_query/query-data-provider";
@@ -29,61 +19,35 @@ export class TextInputComponent implements OnInit {
   @Input() value?: Displayable<unknown>[] = [];
   /** Writable value */
   protected localValue: Displayable<string>[] = [];
+  protected values: string[] = [];
+
+  /** Key codes that is be used to separate words in text input chips mode */
+  separatorExp = /[,，、\s\t]/;
+  error: string | null = null;
 
   ngOnInit(): void {
-    this.inputControl.valueChanges.subscribe(() => this.setInputError(null));
+    this.localValue = (this.value as Displayable<string>[]) ?? [];
+    this.values = this.localValue.map(value => value.value as string);
   }
 
   notifyQueryUpdate() {
     this.updated.emit(this.localValue);
   }
 
-  /** Key codes that is be used to separate words in text input chips mode */
-  separatorKeyCodes = [SPACE, ENTER];
-  /** Input of value */
-  inputControl = new FormControl("");
+  onValueChanged() {
+    this.localValue = this.values
+      .map(value => {
+        value = value.trim();
 
-  @ViewChild("chipGrid") chipGrid!: MatChipGrid;
-  error: string | null = null;
+        const error = this.provider?.getValidationResult(value);
+        if (error) {
+          this.error = error;
+          return null;
+        }
 
-  /**
-   * Set input error to display or clear error
-   * @param error error message to set, if null, will clear error
-   */
-  setInputError(error: string | null) {
-    this.chipGrid.errorState = !!error;
-    this.error = error;
-  }
-
-  /**
-   * Add text in input to query value and clear input
-   * @param event event of input sent by MatChipInput
-   */
-  addText(event: MatChipInputEvent): void {
-    if (this.provider?.type !== "text") return;
-
-    const value = event.value.trim();
-    if (value === "") return;
-
-    const error = this.provider.getValidationResult(value);
-    this.setInputError(error);
-    if (error) return;
-
-    if (!this.localValue) this.localValue = [];
-    this.localValue.push({
-      value: event.value.trim(),
-      label: event.value.trim(),
-    });
-    this.inputControl.setValue("");
-    this.notifyQueryUpdate();
-  }
-
-  /**
-   * Remove selected option from query value
-   * @param index index of selected option to remove
-   */
-  removeSelectedOption(index: number): void {
-    this.localValue?.splice(index, 1);
+        return { value, label: value };
+      })
+      .filter(value => value !== null) as Displayable<string>[];
     this.notifyQueryUpdate();
   }
 }
--- src/app/course/course-search-helper/text-input/text-input.module.ts
+++ src/app/course/course-search-helper/text-input/text-input.module.ts
@@ -1,24 +1,13 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { MatChipsModule } from "@angular/material/chips";
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatIconModule } from "@angular/material/icon";
-import { MatInputModule } from "@angular/material/input";
+import { ChipsModule } from "primeng/chips";
 
 import { TextInputComponent } from "./text-input.component";
 
 @NgModule({
   declarations: [TextInputComponent],
-  imports: [
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MatChipsModule,
-    MatFormFieldModule,
-    MatIconModule,
-    MatInputModule,
-  ],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, ChipsModule],
   exports: [TextInputComponent],
 })
 export class TextInputModule {}
