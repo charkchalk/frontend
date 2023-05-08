3d8f3890d0b4581eb24d6b2f03108d07f5da80ad
moontai0724 <moontai0724@gmail.com>
2023-05-08 22:21:59 +0800

    feat: form control for text input

--- src/app/course/course-search-helper/text-input/text-input.component.ts
+++ src/app/course/course-search-helper/text-input/text-input.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { AbstractControl, FormControl, Validators } from "@angular/forms";
+import { Observable } from "rxjs";
 
 import { Displayable } from "../../../_types/displayable";
 import { QueryDataProvider } from "../../_query/query-data-provider";
@@ -15,28 +17,33 @@ export class TextInputComponent implements OnInit {
   @Output() updated: EventEmitter<Displayable<string>[]> = new EventEmitter();
   /** Data provider of current filtering condition */
   @Input() provider?: QueryDataProvider;
+  /** An event emitter that receive event from parent when provider changed */
+  @Input() providerChange?: Observable<void>;
   /** Value of current filtering condition */
   @Input() value?: Displayable<unknown>[] = [];
-  /** Writable value */
-  protected localValue: Displayable<string>[] = [];
-  protected values: string[] = [];
 
   /** Key codes that is be used to separate words in text input chips mode */
   separatorExp = /[,，、\s\t]/;
   error: string | null = null;
 
-  ngOnInit(): void {
-    this.localValue = (this.value as Displayable<string>[]) ?? [];
-    this.values = this.localValue.map(value => value.value as string);
-  }
+  /** Emit control to parent after control initialized */
+  @Output() controlSet = new EventEmitter<AbstractControl>();
+  /** Control of current filtering condition */
+  control = new FormControl<string[]>([], Validators.required);
 
-  notifyQueryUpdate() {
-    this.updated.emit(this.localValue);
+  ngOnInit(): void {
+    const values = (this.value as Displayable<string>[]) || [];
+    this.control.setValue(values.map(value => value.value as string));
+    this.controlSet.emit(this.control);
+    if (!this.provider) this.control.disable();
+
+    this.providerChange?.subscribe(() => this.control.enable());
+    this.control.valueChanges.subscribe(() => this.onValueChanged());
   }
 
   onValueChanged() {
-    this.localValue = this.values
-      .map(value => {
+    const values = this.control.value
+      ?.map(value => {
         value = value.trim();
 
         const error = this.provider?.getValidationResult(value);
@@ -48,6 +55,6 @@ export class TextInputComponent implements OnInit {
         return { value, label: value };
       })
       .filter(value => value !== null) as Displayable<string>[];
-    this.notifyQueryUpdate();
+    this.updated.emit(values);
   }
 }
